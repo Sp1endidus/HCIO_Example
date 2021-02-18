ef4606ec150c6cf7b5f10477018aea974d2a74ee
Your Name <you@example.com>
2021-02-18 21:06:27 +0300

    init

---
+++ Assets/CS/Scripts/BodyManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyManager : MonoBehaviour
+{
+    public BezierSolution.BezierWalkerWithSpeed BezierWalkerWithSpeed;
+    public int CubesCount;
+
+
+    public float BoundXMin = -2f;
+    public float BoundXMax = 2f;
+    public float MoveSpeed = 0.01f;
+
+    Vector3 lastMousePos;
+    Vector3 newPosBuffer;
+    float horizontalBuffer;
+
+
+    private void Start()
+    {
+        foreach (var item in GetComponentsInChildren<CubeManager>())
+        {
+            AddCube(item);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            lastMousePos = Input.mousePosition;
+            BezierWalkerWithSpeed.enabled = true;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            horizontalBuffer = Input.mousePosition.x - lastMousePos.x;
+
+            newPosBuffer.x = transform.localPosition.x + (MoveSpeed * horizontalBuffer);
+            newPosBuffer.x = Mathf.Clamp(newPosBuffer.x, BoundXMin, BoundXMax);
+
+            transform.localPosition = newPosBuffer;
+            lastMousePos = Input.mousePosition;
+        }
+
+        if (CubesCount <= 0)
+        {
+            BezierWalkerWithSpeed.enabled = false;
+        }
+    }
+
+    public void AddCube(CubeManager cubeManager)
+    {
+        CubesCount++;
+
+        cubeManager.InBody = true;
+        cubeManager.BodyManager = this;
+        cubeManager.transform.SetParent(transform);
+        cubeManager.col.isTrigger = false;
+        cubeManager.rb.isKinematic = false;
+        cubeManager.transform.position = new Vector3(transform.position.x, CubesCount - 0.5f, transform.position.z);
+    }
+
+    public void RemoveCube(CubeManager cubeManager)
+    {
+        if (cubeManager.InBody)
+        {
+            CubesCount--;
+
+            cubeManager.InBody = false;
+            cubeManager.transform.SetParent(null);
+        }
+    }
+}
---
+++ Assets/CS/Scripts/CubeManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public float MagnitudeThreshold = 0.1f;
+    public float BelowZeroValue = 0.4f;
+
+    public Rigidbody rb { get; private set; }
+    public Collider col { get; private set; }
+
+    public BodyManager BodyManager;
+    public bool InBody { get; set; }
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (InBody)
+            return;
+
+        if (other.transform.parent)
+        {
+            BodyManager = other.transform.parent.GetComponent<BodyManager>();
+            if (BodyManager)
+            {
+                BodyManager.AddCube(this);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (InBody)
+        {
+            if (VelocityHorizontalChanged || BelowZero)
+            {
+                BodyManager.RemoveCube(this);
+            }
+        }
+    }
+
+    public bool VelocityHorizontalChanged
+        => Mathf.Abs(rb.velocity.x) > MagnitudeThreshold
+        || Mathf.Abs(rb.velocity.z) > MagnitudeThreshold;
+
+
+    public bool BelowZero => transform.position.y < BelowZeroValue;
+}
